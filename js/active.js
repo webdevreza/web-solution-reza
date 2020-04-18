b3f63f8183b9846f75fe9c707e97035ea505ac84
Rezaul-karim-pstu <63894053+Rezaul-karim-pstu@users.noreply.github.com>
2020-04-18 11:05:15 +0600

    Add files via upload

---
+++ js/active.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+  
+$('.slider-active').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:true,
+    autoplay:true,
+    smartSpeed:1000,
+    items:1,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+});
+    
+$('.testi-active').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:true,
+    autoplay:true,
+    smartSpeed:1000,
+    items:1,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+});
+
+
+
+
+});
